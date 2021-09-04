91c95f39efc7e22661cc9019ffc140b66f606cc3
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-09-04 20:53:57 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -31,7 +31,7 @@ from pyrogram.errors import UserNotParticipant
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id not in Config.BANNED_USERS:
         logger.info(update.from_user)
         url = update.text
         youtube_dl_username = None
